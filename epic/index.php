bb766eb57bf9fc9a3c0d9ef86a30629782b722d3
NickSchnepple <nschnepple@cnm.edu>
2019-04-09 16:31:05 -0600

    added title and headers

---
+++ epic/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+		<meta charset="UTF-8">
+		<title>Data Design Project</title>
+</head>
+
+<body>
+			<h1>Data Design Project</h1>
+			<h2>Persona</h2>
+</body>
+
+
+</html>
