23199328e324e4270d213815f4d4b7a1543106aa
NickSchnepple <nschnepple@cnm.edu>
2019-04-09 19:48:21 -0600

    Added new image and made list about "Austin Martin".

--- epic/index.php
+++ epic/index.php
@@ -1,14 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+	<head>
 		<meta charset="UTF-8">
-		<title>Data Design Project</title>
-</head>
+	<title>Data Design Project</title>
+	</head>
 
-<body>
-			<h1>Data Design Project</h1>
-			<h2>Persona</h2>
-			<img src="image.jpg" height="300" width="350" alt="Smiley Face">
+	<body>
+		<h1>Data Design Project</h1>
+
+		<h2>Persona</h2>
+
+		<img src="Austin.jpg" height="256" width="244" alt="Smiley Face"
+				<ul>
+					<li><strong>Name:</strong> <em>Austin Martin</em></li>
+					<li><strong>Occupation:</strong> <em>Artists</em></li>
+					<li><strong>Age:</strong> <em>28 Years Old</em></li>
+					<li><strong>Technology:</strong> <em>SmartPhone</em></li>
+					<li><strong>Technical Proficiency:</strong> <em>10/10</em></li>
+					<li><strong>Gender:</strong> <em>Male</em></li>
+				</ul>
+
+				<h3>Interests and Hobbies</h3>
+					<ul>
+					<li>Loves to be outdoors.</li>
+					<li>Volunteers at the Soup Kitchen, and dog pound.</li>
+					<li>Enjoys Skateboarding.</li>
+					<li>Likes to drive fast cars.</li>
+					</ul>
+
 </body>
 
 
