d364f03283737a9d2ca8ebb34ae556e53f10d21e
NickSchnepple <nschnepple@cnm.edu>
2019-04-13 13:40:33 -0600

    fixed database tables.

--- epic/data-design/sql/data-design-project.sql
+++ epic/data-design/sql/data-design-project.sql
@@ -1,33 +1,44 @@
+set foreign_key_checks=0; drop table customer; set foreign_key_checks=1;
+drop table if exists customer;
+drop table if exists orderItem;
+drop table if exists customerOrder;
+drop table if exists item;
+
+
 create table customer (
-   customerId binary(16) not null,
-   customerFirstName varchar(128) not null,
-   customerLastName varchar(128) not null,
-   customerAddress varchar(64) not null,
-   customerCity varchar(64) not null,
-   customerPhoneNumber varchar(16),
-   index(customerFirstName),
-   primary key(customerId)
+	customerId binary(16) not null,
+	customerFirstName varchar(128) not null,
+	customerLastName varchar(128) not null,
+	customerAddress varchar(64) not null,
+	customerCity varchar(64) not null,
+	customerPhoneNumber varchar(16),
+	primary key(customerId),
+	index(customerId)
 );
 
-create table 'order' (
-   orderId binary(16) not null,
-   orderCustomerId varchar(32) not null,
-   orderDateShipped varchar(16) not null,
-   primary key(orderId),
-   foreign key(orderCustomerId) references customer (customerId)
-);
 
 create table item (
-   itemId binary(16) not null,
-   itemCost varchar(64) not null,
-   itemName varchar(128) not null,
-   itemQuantity varchar(32) not null,
-   primary key(itemId)
+	itemId binary(16) not null,
+	itemCost varchar(64) not null,
+	itemName varchar(128) not null,
+	itemQuantity varchar(32) not null,
+	primary key(itemId),
+	index(itemId)
+);
+
+create table customerOrder (
+	customerOrderCustomerId binary(16) not null,
+	orderId varchar(32) not null,
+	orderDateShipped varchar(16) not null,
+	index(orderId),
+	index(customerOrderCustomerId),
+	primary key(orderId),
+	foreign key(customerOrderCustomerId) references customer(customerId)
 );
 
 create table orderItem (
-   orderItemItemId binary(16) not null,
-   orderItemOrderId binary(16) not null,
-   foreign key(orderItemItemId) references item (itemId),
-   foreign key(orderItemOrderId) references 'order'(orderId)
+	orderItemItemId  binary(16) not null,
+	orderItemOrderId varchar(32) not null,
+	foreign key (orderItemItemId) references item(itemId),
+	foreign key (orderItemOrderId) references customerOrder(orderId)
 );
