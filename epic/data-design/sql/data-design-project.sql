ac9162a72838e4ca4ea57d7f0b4421a171786cca
NickSchnepple <nschnepple@cnm.edu>
2019-04-12 18:16:10 -0600

    moved content to correct file

---
+++ epic/data-design/sql/data-design-project.sql
@@ -0,0 +1,33 @@
+create table customer (
+   customerId binary(16) not null,
+   customerFirstName varchar(128) not null,
+   customerLastName varchar(128) not null,
+   customerAdress varchar(64) not null,
+   customerCity varchar(64) not null,
+   customerPhoneNumber varchar(16),
+   index(customerFirstName),
+   primary key(customerId)
+);
+
+create table 'order' (
+   orderId binary(16) not null,
+   orderCustomerId varchar(32) not null,
+   orderDateShipped varchar(16) not null,
+   primary key(orderId),
+   foreign key(orderCustomerId) references customer (customerId)
+);
+
+create table item (
+   itemId binary(16) not null,
+   itemCost varchar(64) not null,
+   itemName varchar(128) not null,
+   itemQuantity varchar(32) not null,
+   primary key(itemId)
+);
+
+create table orderItem (
+   orderItemItemId binary(16),
+   orderItemOrderId binary(16),
+   foreign key(orderItemItemId) references item (itemId),
+   foreign key(orderItemOrderId) references 'order' (orderId)
+);
